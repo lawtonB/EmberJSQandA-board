766a265800d415c6a4f3b1ab5c18d9bc501a3844
lawton browning <lawton.a.browning@gmail.com>
2016-04-01 12:08:34 -0700

    add functional answer on main page, working on switching over answer to question-detail

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question'),
+      answers: this.store.findAll('answer')
+    });
   },
 
   actions: {
@@ -19,6 +22,12 @@ export default Ember.Route.extend({
         }
       });
       question.save();
+    },
+
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
+      this.transitionTo('index');
     },
 
     destroyQuestion(question) {
