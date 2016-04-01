70513f01da76b0e4f48af26d0f9cd39f5b777804
lawton browning <lawton.a.browning@gmail.com>
2016-04-01 09:47:19 -0700

    add update function

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
+
+    update(question, params) {
+      this.sendAction('update', question, params);
+    },
+
     delete(question) {
       if (confirm('delete question?')) {
         this.sendAction('destroyQuestion', question);
