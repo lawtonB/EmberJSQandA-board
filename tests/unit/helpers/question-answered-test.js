6978ae1520acc451d9df1a8b8ded7914f7486fe8
lawton browning <lawton.a.browning@gmail.com>
2016-04-08 08:54:16 -0700

    add basic structure for questionanswered helper

---
+++ tests/unit/helpers/question-answered-test.js
@@ -0,0 +1,10 @@
+import { questionAnswered } from 'qand-a-board/helpers/question-answered';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | question answered');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = questionAnswered([42]);
+  assert.ok(result);
+});
