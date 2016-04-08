d330b123bb12e3117c43513e39986cd42f059dd3
lawton browning <lawton.a.browning@gmail.com>
2016-04-08 10:24:31 -0700

    add sort answer by date

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['date:asc'],
+  sortedAnswers: Ember.computed.sort('question.answers', 'sortBy'),
+
   actions: {
     delete(answer) {
       if (confirm('delete answer?')) {
